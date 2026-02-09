2983a9eec8c76e4dc9336e46d046d322332697ea
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-02-09 17:53:04 +0545

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function handleMenuClick() {
+    // const sidePanel = document.getElementsByClassName("side-panel")
+    // sidePanel[0].style.animation = "sidePanel 0.5 ease 0.1"
+    // sidePanel[0].style.display = "block"
+    alert("Please use desktop if you want site navigation!")
+}
+
